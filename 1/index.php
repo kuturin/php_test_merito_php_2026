67cff259394f2f3e4b9eee90ff66eb79de94b96a
kuturin <turkurun2@gmail.com>
2026-09-20 15:51:03 +0200

    1

--- 1/index.php
+++ 1/index.php
@@ -1,2 +1,22 @@
 <?php
-    echo "test php";
+require_once "classes/Student.php";
+
+$student1 = new Student("Jan", 18, "ZSK");
+$student2 = new Student("Anna", 17, "ZSŁ");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
