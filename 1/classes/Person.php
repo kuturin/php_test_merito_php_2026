56e291c6ca0c05fbcf77df013409d4e298863ea5
kuturin <turkurun2@gmail.com>
2026-09-20 15:42:04 +0200

    1 zajęcia

---
+++ 1/classes/Person.php
@@ -0,0 +1,18 @@
+<?php
+class Person
+{
+    protected string $name;
+    protected int $age;
+
+    public function __construct(string $name, int $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function getInfo(): string
+    {
+        return "Imię {$this->name}, wiek ($this->age}";
+
+    }
+}
