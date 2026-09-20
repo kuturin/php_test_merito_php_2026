9274db9792be63c38ecb599bb2cfa17fd9ac865f
kuturin <turkurun2@gmail.com>
2026-09-20 16:15:30 +0200

    klasa nauczyciel

---
+++ 1/classes/Teacher.php
@@ -0,0 +1,28 @@
+<?php
+require_once "Person.php";
+
+class Teacher extends Person
+{
+    private string $przedmiot;
+
+    public function __construct(string $name, int $age, string $przedmiot)
+    {
+        parent::__construct($name, $age);
+        $this->przedmiot = $przedmiot;
+    }
+    public function getInfo(): string
+    {
+        return "Imię {$this->name}, wiek ($this->age}, przedmiot: {$this->przedmiot}";
+
+    }
+    public function getHtml(): string
+    {
+        return <<<HTML
+        <div class="teeacher">
+            <h2>{this->name}</h2>
+            <p>Wiek: {this->age}</p>
+            <p>Przedmiot {this->przedmiot}</p>
+</div>
+HTML;
+    }
+}
--- 1/index.php
+++ 1/index.php
@@ -1,8 +1,10 @@
 <?php
 require_once "classes/Student.php";
+require_once "classes/Teacher.php";
 
 $student1 = new Student("Jan", 18, "ZSK");
 $student2 = new Student("Anna", 17, "ZSŁ");
+$teacher1 = new Teacher("Piotr", 34, "Informatyka");
 
 ?>
 
@@ -24,12 +26,19 @@ $student2 = new Student("Anna", 17, "ZSŁ");
     echo $student2->getInfo() . "<hr>";
     ?>
 
+    <h1>Nauczyciele</h1>
+    <h2>Informacje o użytkownikach</h2>
+    <?php
+    echo $teacher1->getInfo() . "<hr>";
+    ?>
+
     <h2>Informacje HTML - HEREDOC</h2>
 </body>
 
 <?php
 echo $student1->getHtml();
 echo $student2->getHtml();
+echo $teacher1->getHtml();
 
 ?>
 
